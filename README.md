f820d00d3fcf30aa1e5fa042d6456198941fc125
Schalk Burger <schalkb@gmail.com>
2019-11-12 21:42:01 +0200

    Add first version of Hacker News Readable Light userstyle

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# hacker-news-readable-light
+# Hacker News Readable Light
 A more readable version of Hacker News inspired by Instapaper.
+
+[Download from Stylish / Userstyles.org](https://userstyles.org/styles/177181/hacker-news-readable-light)
+
+#
+
+![Image of Yaktocat](hacker-news-readable-light.png)
+
+#
---
+++ hacker-news-readable-light.user.js
@@ -0,0 +1,32 @@
+@-moz-document url-prefix("https://news.ycombinator.com/") {
+    body, .title {
+        font-family: Georgia, Times, Times New Roman, serif;
+    }
+    
+    table#hnmain {
+        max-width: 750px;
+        background-color: transparent;
+    }
+    
+    td {
+        background-color: transparent;
+    }
+    
+    .title {
+        font-size: 14pt;
+        color: #111;
+    }
+    
+    tr.spacer {
+        height: 20px !important;
+        box-shadow: 0px 1px 0 0px #ececec;
+    }
+    
+    tr.athing td {
+        padding-top: 20px;
+    }
+    
+    td.subtext {
+        padding-top: 8px;
+    }
+}
