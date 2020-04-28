c0ff78d5bbb39257623178a0cd4dadadd5932a61
Schalk Burger <schalkb@gmail.com>
2020-04-28 20:05:01 +0200

    Update Hacker News Readable Light styles

--- hacker-news-readable-light.user.js
+++ hacker-news-readable-light.user.js
@@ -1,11 +1,20 @@
 @-moz-document url-prefix("https://news.ycombinator.com/") {
     body, .title {
-        font-family: Georgia, Times, Times New Roman, serif;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        background: #fff !important;
+        color: #939393 !important;
+        }
+    
+    a:link {
+        color: #939393 !important;
+    }
+    a:visited {
+        color: #939393 !important;
     }
     
     table#hnmain {
-        max-width: 750px;
-        background-color: transparent;
+        max-width: 700px;
+        background-color: rgba(125, 124, 124, 0);
     }
     
     td {
@@ -13,8 +22,18 @@
     }
     
     .title {
+        color: #979797 !important;
+    }
+    
+    .title a {
+        font-family: Georgia, Times, Times New Roman, serif;
         font-size: 14pt;
-        color: #111;
+        color: #595959 !important;
+        line-height: 1.4;
+    }
+    
+    .subtext a {
+        color: #939393 !important;
     }
     
     tr.spacer {
@@ -29,15 +48,16 @@
     td.subtext {
         padding-top: 8px;
     }
-
-    .comment-tree .comment, .fatitem .comment {
+    
+    .comment-tree .comment,
+    .fatitem .comment {
         font-family: Georgia, Times, Times New Roman, serif;
         font-size: 12pt;
         line-height: 22pt;
     }
-
+    
     .fatitem {
-        background: #f7f5f5;
+        background: #fff0;
         width: 100%;
         padding: 0 0 20px 0;
         border-bottom: 2px solid #ccc;
@@ -46,5 +66,9 @@
     .fatitem .comment {
         background: #f7f5f5;
         padding: 15px 25px;
+    }
+    
+    .comment .c00 {
+        color: #fff !important;
     }
 }
