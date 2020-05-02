35af602d23f6024e427262d16f566b1cdb1a15ce
Schalk Burger <schalkb@gmail.com>
2020-05-02 21:58:31 +0200

    Update Hacker News Readable Dark style and preview

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # Hacker News Readable Dark
 
-A more readable version of Hacker News inspired by Instapaper.
-
-[Download from Stylish / Userstyles.org](https://userstyles.org/styles/177181/hacker-news-readable-light)
+A more readable, dark version of Hacker News inspired by Instapaper.
 
 #
 
 ![Image of Hacker News Readable Dark](hacker-news-readable-dark.png)
-
-#
--- hacker-news-readable-dark.user.js
+++ hacker-news-readable-dark.user.js
@@ -2,73 +2,83 @@
     body,
     .title {
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-        background: #2D3142 !important;
-        color: #fff !important;
+        background: #141414 !important;
+        color: #b2b6b9 !important;
     }
-    
-    a:link {
-        color: #e6e6e6 !important;
+
+    img {
+        filter: grayscale(1);
     }
-    a:visited {
-        color: #939393 !important;
-    }
-    
+
     table#hnmain {
         max-width: 700px;
         background-color: rgba(125, 124, 124, 0);
     }
-    
+
     td {
         background-color: transparent;
     }
-    
+
     .title {
-        color: #979797 !important;
+        color: #b2b6b9 !important;
     }
-    
+
     .title a {
-        font-family: Georgia, Times, Times New Roman, serif;
-        font-size: 14pt;
-        color: #f9f9f9 !important;
+        font-size: 13pt;
+        color: #b2b6b9 !important;
         line-height: 1.4;
+        font-weight: 500;
     }
-    
+
+    a:link {
+        color: #b2b6b9 !important;
+    }
+    a:visited {
+        color: #4c5053 !important;
+    }
+
+    span.sitebit.comhead a > span {
+        color: #545454 !important;
+        font-size: 10pt;
+    }
+
     .subtext a {
-        color: #b7b7b7 !important;
+        color: #6d6d6d !important;
     }
-    
+
     tr.spacer {
         height: 20px !important;
         box-shadow: 0px 1px 0 0px #ececec;
     }
-    
+
     tr.athing td {
         padding-top: 20px;
+        border-top: 1px solid #232323;
     }
-    
+
     td.subtext {
         padding-top: 8px;
     }
-    
+
     .comment-tree .comment,
     .fatitem .comment {
         font-family: Georgia, Times, Times New Roman, serif;
         font-size: 12pt;
         line-height: 22pt;
     }
-    
+
     .fatitem {
         background: #fff0;
         width: 100%;
         padding: 0 0 20px 0;
         border-bottom: 2px solid #ccc;
     }
-    
+
     .fatitem .comment {
         background: #f7f5f5;
         padding: 15px 25px;
     }
-    
+
     .comment .c00 {
         color: #fff !important;
     }
